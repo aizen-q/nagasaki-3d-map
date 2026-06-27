3d45786e587d50628ae952152ed0713849e21d9f
aizen-q <qqq.qtaro.unya@gmail.com>
2026-06-27 10:52:54 +0900

    Update sw.js v22

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 //               地図タイルはネットワーク優先・フォールバックキャッシュ
 // =============================================
 
-const CACHE_VERSION = 'v21';
+const CACHE_VERSION = 'v22';
 const SHELL_CACHE   = `shell-${CACHE_VERSION}`;
 const TILE_CACHE    = `tiles-${CACHE_VERSION}`;
 
